365fda48c9ece4ea2d934d1e633058766757ced9
Boas Küllmei <87889850+Flofftoff@users.noreply.github.com>
2024-09-05 17:33:46 +0200

    Update till Line 69 roman_calc.py

--- roman_calc.py
+++ roman_calc.py
@@ -1,4 +1,4 @@
-#        Hallo, Stafan, Roland & Andreas, ich hab hier nochmal die Aufgabe von gestern durch gearbeitet und 
+#        Hallo, Stefan, Roland & Andreas, ich hab hier nochmal die Aufgabe von gestern durch gearbeitet und 
 #        habe herrausgefunden, warum das gestern so Probleme gab. Denn man mag es sich kaum vorstellen,
 #        aber ich habe unter dieser Stresssituation tatsächlich vergessen dass If-Else-Statements existieren. 
 #        Ich versuchte durchgehend krampfhaft zu überlegen, wie ich 2 Werte verleich. Das ist mir dann tatsächlich 
@@ -45,29 +45,29 @@ def convert_from_roman(deci_numbers, roman_numbers, input_num): #converting numb
                 subtract = subtract + d_num_raw[number] #+1 to compensate stating to count at 0
 
         value = 0 #initialising variable
-        for number in range(biggest_number_index, len(d_num_raw)): # adding numbers after and including biggest number
+        for number in range(biggest_number_index, len(d_num_raw)): # adding numbers after, and including the biggest number
             value += d_num_raw[number]
 
-        d_num_final = value - subtract #subtracting suffix (if needed)
+        d_num_final = value - subtract #subtracting predfix (if needed, if not, i subtracts 0)
         converted_numbers.append(d_num_final) #appending converted number to list and start over
     return(converted_numbers)   
 
-def calc(numbers):
+def calc(numbers): #simple method for adding all the nubers together
     result = 0
-    for i in numbers: #simply add every number together 
+    for i in numbers: # itterating through every number and adding it 
         result += i
     print(f"Das Ergebnis ist {result} (in Dezimal)")
     return(result)
 
 def conversion(deci_numbers, roman_numbers, roman_list, deci_result, index): #method to convert the numbers back to decimal, could also be done with 6 if-statements, but i decided to do it dynamicly with the try-except method, so it will fail, get catched, and start over with the next index
-    times_index = deci_result // deci_numbers[index] #deviding while rounding down
+    times_index = deci_result // deci_numbers[index] #deviding while rounding down (using //)
     for i in range(times_index): #adding the letter to a list, how many times it is available
         roman_list.append(roman_numbers[index])
-    deci_result = deci_result % deci_numbers[index]
-    return(roman_list, deci_result)
+    deci_result = deci_result % deci_numbers[index] # using Modulo to get the remainder of the devision
+    return(roman_list, deci_result) # and pass it on to the next method
 
 def convert_from_deci(deci_numbers, roman_numbers, deci_result):
-    roman_list = []    
+    roman_list = [] # init list to add every single letter, later it will be merged  to a string
     for i in reversed(range(len(roman_numbers))): # getting length of list of roman numbers, in this case 6. reverse it and use it in a for loop
         try:
             roman_list, deci_result = conversion(deci_numbers, roman_numbers, roman_list, deci_result, i) # index gets passed, if index is 6, it devides by 1000 and uses letter M cause both are at index 6. Then counting down until were deviding by 1, and finishes
